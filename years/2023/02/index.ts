4a80b9462d00e15a3ad8878afb2d382f2f84ad1b
Todd.Southen <toddsouthenbentley@users.noreply.github.com>
2023-12-06 18:41:18 -0800

    Day 2 completed

--- years/2023/02/index.ts
+++ years/2023/02/index.ts
@@ -13,17 +13,75 @@ const DAY = 2;
 // data path    : /Users/todd/macdev/personal/advent-of-code/years/2023/02/data.txt
 // problem url  : https://adventofcode.com/2023/day/2
 
+type DieColor = "red" | "green" | "blue";
+
+class Draw {
+	counts = new Map<DieColor, number>();
+
+	constructor(line: string) {
+		line.split(", ").forEach((cube) => {
+			const [num, color] = cube.split(" ");
+			this.counts.set(color as DieColor, Number(num));
+		});
+	}
+}
+
+class Game {
+	id = 0;
+	draws: Draw[] = [];
+
+	constructor(line: string) {
+		const [game, draws] = line.split(": ");
+		this.id = Number(game.split(" ")[1]);
+		draws.split("; ").forEach((draw) => this.draws.push(new Draw(draw)));
+	}
+
+	getMaxCount(color: DieColor) {
+		let count = 0;
+		this.draws.forEach((draw) => count = Math.max(count, draw.counts.get(color) ?? 0));
+		return count;
+	}
+}
+
 async function p2023day2_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const games: Game[] = [];
+	input.split("\n").forEach((line) => games.push(new Game(line)));
+	let sum = 0;
+	games.forEach((game) => {
+		if (game.getMaxCount("red") <= 12 && game.getMaxCount("green") <= 13 && game.getMaxCount("blue") <= 14)
+			sum += game.id;
+	});
+	return sum.toString();
 }
 
 async function p2023day2_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const games: Game[] = [];
+	input.split("\n").forEach((line) => games.push(new Game(line)));
+	let sum = 0;
+	games.forEach((game) => {
+		const power = game.getMaxCount("red") * game.getMaxCount("green") * game.getMaxCount("blue");
+		sum += power;
+	});
+	return sum.toString();
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
+		expected: "8",
+	}];
+	const part2tests: TestCase[] = [{
+		input: `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
+		expected: "2286",
+	}];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
 
