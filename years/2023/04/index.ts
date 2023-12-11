b244ef162e8dad4d9258fc295bb56656cebaa03b
Todd.Southen <toddsouthenbentley@users.noreply.github.com>
2023-12-10 23:42:21 -0800

    Day 4 completed

--- years/2023/04/index.ts
+++ years/2023/04/index.ts
@@ -5,6 +5,7 @@ import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
 import { normalizeTestCases } from "../../../util/test";
+import * as R from "ramda";
 
 const YEAR = 2023;
 const DAY = 4;
@@ -13,17 +14,68 @@ const DAY = 4;
 // data path    : /Users/todd/macdev/personal/advent-of-code/years/2023/04/data.txt
 // problem url  : https://adventofcode.com/2023/day/4
 
+type Card = {
+	matches: number;
+	count: number;
+};
+
+function parseCard(input: string): Card {
+	const splits = input.split(/[|:]/);
+	const [_card, winners, nums] = splits;
+	const winningNumbers = winners.trim().split(/ +/).map(Number);
+	const numbers = nums.trim().split(/ +/).map(Number);
+	const matches = R.length(R.intersection(winningNumbers, numbers));
+	return { matches, count: 1};
+}
+
+
 async function p2023day4_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const score = (card: Card) => card.matches > 0 ? Math.pow(2, card.matches - 1) : 0;
+	const sum = R.pipe(
+		R.split("\n"),
+		R.map((line) => parseCard(line)),
+		R.map((card) => score(card)),
+		R.sum,
+	)(input);
+	return sum.toString();
 }
 
 async function p2023day4_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const sum = R.pipe(
+		R.split("\n"),
+		R.map((line) => parseCard(line)),
+		(cards) => {
+			cards.forEach((card, index) => {
+				if (card.matches > 0)
+					cards.slice(index + 1, index + 1 + card.matches).forEach((c) => c.count += card.count);
+			});
+			return cards;
+		},
+		R.map(R.prop("count")),
+		R.sum,
+	)(input);
+	return sum.toString();
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+		expected: "13",
+	}];
+	const part2tests: TestCase[] = [{
+		input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+		expected: "30",
+	}];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
 
