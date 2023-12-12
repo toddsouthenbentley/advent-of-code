a602d5adc30f0eb956d6548aa980210899e8b02f
Todd.Southen <toddsouthenbentley@users.noreply.github.com>
2023-12-11 20:23:01 -0800

    Day 5 completed

--- years/2023/05/index.ts
+++ years/2023/05/index.ts
@@ -5,6 +5,8 @@ import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
 import { normalizeTestCases } from "../../../util/test";
+import * as R from "ramda";
+import { assert } from "console";
 
 const YEAR = 2023;
 const DAY = 5;
@@ -13,17 +15,149 @@ const DAY = 5;
 // data path    : /Users/todd/macdev/personal/advent-of-code/years/2023/05/data.txt
 // problem url  : https://adventofcode.com/2023/day/5
 
+type RangeMap = {
+	fromName: string;
+	toName: string;
+	ranges: number[][];
+}
+
+type InputData = {
+	seeds: number[];
+	maps: RangeMap[];
+}
+
+function parseRangeMap(input: string) {
+	const [nameLine, mapLines] = input.split(" map:\n");
+	const [fromName, _to, toName] = nameLine.split("-");
+	const ranges = R.pipe(
+		R.split("\n"),
+		R.map(R.pipe(R.split(" "), R.map(Number))),
+	)(mapLines);
+	return { fromName, toName, ranges};
+}
+
+function parseInput(input: string): InputData {
+	return R.pipe(
+		R.split("\n\n"),
+		(sections) => {
+			const [_seeds, seedNums] = sections.shift()!.split(": ");
+			const seeds = seedNums.split(" ").map(Number);
+			const maps = sections.map((section) => parseRangeMap(section));
+			return { seeds, maps };
+		}
+	)(input);
+}
+
+function getMappedValue(val: number, rangeMap: RangeMap, reverse = false) {
+	const checkIdx = reverse ? 0 : 1;
+	const mapIdx = reverse ? 1 : 0;
+	const range = rangeMap.ranges.find((r) => {
+		return val >= r[checkIdx] && val < (r[checkIdx] + r[2]);
+	});
+	return range ? range[mapIdx] + (val - range[checkIdx]) : val;
+}
+
 async function p2023day5_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const data = parseInput(input);
+	const mappedVals = data.seeds.map((val) => data.maps.reduce((acc, rangeMap) => getMappedValue(acc, rangeMap), val));
+	const minVal = Math.min(...mappedVals);
+	return minVal.toString();
 }
 
 async function p2023day5_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const data = parseInput(input);
+	const seeds = R.splitEvery(2, data.seeds);
+	const lastMap = data.maps[data.maps.length-1];
+	lastMap.ranges.sort((a, b) => a[1] - b[1]);
+	data.maps.reverse();
+
+	// what is the theoretical max? Could be anything, couldn't it?
+	const min = 1;
+	const max = seeds.reduce((acc, [start, count]) => Math.max(start + count, acc), 0);
+
+	for (let val = min; val < max; val++) {
+		const seedVal = data.maps.reduce((acc, rangeMap) => getMappedValue(acc, rangeMap, true), val);
+		const found = seeds.find(([start, count]) => seedVal >= start && seedVal < (start + count))
+		if (found) {
+			return val.toString();
+		}
+	}
+	return "Not found";
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`,
+		expected: "35"
+	}];
+	const part2tests: TestCase[] = [{
+		input: `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`,
+		expected: "46"
+	}];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
 
