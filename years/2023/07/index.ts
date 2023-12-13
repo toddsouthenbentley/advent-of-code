48eb9cd9e9200b4166a18265085a75c51b11814b
Todd Southen <tsouthen@gmail.com>
2023-12-13 00:23:44 -0800

    Day 7 part 2 WIP

--- years/2023/07/index.ts
+++ years/2023/07/index.ts
@@ -24,8 +24,10 @@ enum HandType {
 	HighCard = 0,
 }
 
-function getHandRank(hand: string): HandType {
-	const counts = R.sort((a, b) => b - a, R.values(R.countBy(R.toUpper, hand.split(""))));
+function getHandRank(hand: string, jokers = false): HandType {
+	const handToCheck = jokers ? replaceJokers(hand) : hand;
+	const letterCounts = R.countBy(R.toUpper, handToCheck.split(""));
+	const counts = R.sort((a, b) => b - a, R.values(letterCounts));
 	switch (counts.length) {
 		case 1:
 			return HandType.FiveOfAKind;
@@ -50,24 +52,24 @@ function getHandRank(hand: string): HandType {
 	return HandType.HighCard;
 }
 
-function getCardValues(hand: string, jokers = false) {
-	const vals = R.pipe(
-		R.split(""),
-		R.map(R.compose(
-			R.replace(/A/g, "14"),
-			R.replace(/K/g, "13"),
-			R.replace(/Q/g, "12"),
-			R.replace(/J/g, jokers ? "1": "11"),
-			R.replace(/T/g, "10"))
-		),
-		R.map(Number)
-	)(hand);
-	return vals;
-}
+function compareHands(h1: string, h2: string, jokers = false) {
+	function getCardValues(hand: string) {
+		const vals = R.pipe(
+			R.split(""),
+			R.map(R.compose(
+				R.replace(/A/g, "14"),
+				R.replace(/K/g, "13"),
+				R.replace(/Q/g, "12"),
+				R.replace(/J/g, jokers ? "1": "11"),
+				R.replace(/T/g, "10"))
+			),
+			R.map(Number)
+		)(hand);
+		return vals;
+	}
 
-function compareHands(h1: string, h2: string) {
-	const s1 = getHandRank(h1);
-	const s2 = getHandRank(h2);
+	const s1 = getHandRank(h1, jokers);
+	const s2 = getHandRank(h2, jokers);
 	if (s1 !== s2) return s1 - s2;
 	const diffs = R.pipe(
 		R.map(getCardValues),
@@ -82,10 +84,8 @@ async function p2023day7_part1(input: string, ...params: any[]) {
 	const result = R.pipe(
 		R.split("\n"),
 		R.map(R.split(" ")),
-		R.sort((r1, r2) => {
-			return compareHands(r1[0], r2[0]);
-		}),
-		R.map((r) => r[1]),
+		R.sort((r1, r2) => compareHands(r1[0], r2[0])),
+		R.pluck(1),
 		R.map(Number),
 		(bids) => bids.map((bid, index) => bid * (index + 1)),
 		R.sum,
@@ -93,8 +93,54 @@ async function p2023day7_part1(input: string, ...params: any[]) {
 	return result.toString();
 }
 
+// function cardValue(card: string, jokers = true) {
+// 	switch(card) {
+// 		case "A": return 14;
+// 		case "K": return 13;
+// 		case "Q": return 12;
+// 		case "J": return jokers ? 1 : 11;
+// 		case "T": return 10;
+// 		default: return Number(card);
+// 	}
+// }
+
+// function compareCards(c1: string, c2: string) {
+// 	return cardValue(c1) - cardValue(c2);
+// }
+
+function replaceJokers(hand: string) {
+	// find the card with the highest count, then the highest rank
+	const countsByCard = R.countBy(R.toUpper, hand.split(""));
+	if (!countsByCard["J"])
+		return hand;
+	const withoutJ = R.omit(["J"], countsByCard);
+	const highestCount = R.pipe(
+		R.toPairs,
+		R.sort(([card1, count1]: [string, number], [card2, count2]: [string, number]) => {
+			const countDiff = count1 - count2;
+			return countDiff;
+			// if (countDiff !== 0) return countDiff;
+			// return compareCards(card1, card2);
+		}),
+		R.takeLast(1),
+		R.flatten,
+	)(withoutJ);
+	const newHand = hand.replaceAll("J", highestCount[0] as string);
+	return newHand;
+}
+
 async function p2023day7_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const result = R.pipe(
+		R.split("\n"),
+		R.map(R.split(" ")),
+ 	 	R.sort((r1, r2) => compareHands(r1[0], r2[0], true)),
+		// R.tap((x) => log(x)),
+		R.pluck(1),
+		R.map(Number),
+		(bids) => bids.map((bid, index) => bid * (index + 1)),
+		R.sum,
+	)(input);
+	return result.toString();
 }
 
 async function run() {
@@ -106,7 +152,14 @@ KTJJT 220
 QQQJA 483`,
 		expected: "6440"
 	}];
-	const part2tests: TestCase[] = [];
+	const part2tests: TestCase[] = [{
+		input: `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`,
+		expected: "5905"
+	}];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
 
