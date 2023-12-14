55f940d469cfdda508d548cbaa477c2fcbaa3a3b
Todd Southen <tsouthen@gmail.com>
2023-12-13 17:24:15 -0800

    Day 8 p1 and p2 wip

--- years/2023/08/index.ts
+++ years/2023/08/index.ts
@@ -5,6 +5,7 @@ import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
 import { normalizeTestCases } from "../../../util/test";
+import * as R from "ramda";
 
 const YEAR = 2023;
 const DAY = 8;
@@ -13,17 +14,99 @@ const DAY = 8;
 // data path    : /Users/todd/macdev/personal/advent-of-code/years/2023/08/data.txt
 // problem url  : https://adventofcode.com/2023/day/8
 
+type MapNode = {
+	label: string;
+	index: number;
+	leftNode: MapNode | undefined;
+	rightNode: MapNode | undefined;
+}
+
+function loadNodes(input: string) {
+	const nodes = new Map<string, MapNode>();
+	const cleanedUp = input.replaceAll(/[ ()]/g, "").split("\n");
+	const getOrCreateNode = (label: string, index: number) => {
+		let node = nodes.get(label);
+		if (!node) {
+			node = { label, index, leftNode: undefined, rightNode: undefined};
+			nodes.set(label, node);
+		}
+		return node;
+	}
+	cleanedUp.forEach((line, index) => {
+		const [label, left, right] = line.split(/[=,]/g);
+		const node = getOrCreateNode(label, index);
+		node.leftNode = getOrCreateNode(left, index);
+		node.rightNode = getOrCreateNode(right, index);
+	});
+	return nodes;
+}
+
 async function p2023day8_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const [directions, nodeInput] = input.split("\n\n");
+	const nodes = loadNodes(nodeInput);
+	let currNode = nodes.get("AAA");
+	let steps = 0;
+	let currDirection = 0;
+	while (currNode && currNode.label !== "ZZZ") {
+		currNode = directions[currDirection] === "L" ? currNode.leftNode : currNode.rightNode;
+		steps++;
+		currDirection = (currDirection + 1) % directions.length;
+	}
+	return steps.toString();
 }
 
 async function p2023day8_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const [directions, nodeInput] = input.split("\n\n");
+	const nodes = loadNodes(nodeInput);
+	let currNodes = [...nodes.values()].filter((n) => n.label.endsWith("A")).sort((n1, n2) => n1.index - n2.index);
+	const allEndInZ = (nodeList: MapNode[]) => nodeList.every((n) => n.label.endsWith("Z"));
+	let steps = 0;
+	let currDirection = 0;
+
+	while (steps < 100000000 && !allEndInZ(currNodes)) {
+		currNodes = currNodes.map((n) => directions[currDirection] === "L" ? n.leftNode! : n.rightNode!)
+		steps++;
+		currDirection = (currDirection + 1) % directions.length;
+	}
+	return steps.toString();
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+		input: `RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)`,
+		expected: "2"
+		},
+		{
+		input: `LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)`,
+		expected: "6",
+		}
+	];
+	const part2tests: TestCase[] = [{
+		input: `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`,
+		expected: "6",
+	}];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
 
