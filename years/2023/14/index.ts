04140d9d833813d03e1dcbac386597726d959e98
Todd Southen <tsouthen@gmail.com>
2023-12-20 13:33:12 -0800

    Removed tiltGrid2 since it wasn't any faster.

--- years/2023/14/index.ts
+++ years/2023/14/index.ts
@@ -16,108 +16,25 @@ const DAY = 14;
 // problem url  : https://adventofcode.com/2023/day/14
 
 type Direction = "N" | "E" | "S" | "W";
+
 function tiltGrid(grid: Grid, direction: Direction) {
-	switch (direction) {
-		case "E":
-			grid = grid.rotate(1, "CCW");
-			break;
-		case "W":
-			grid = grid.rotate(1, "CW");
-			break;
-		case "S":
-			grid = grid.rotate(2, "CW");
-			break;
-	}
+	if (direction !== "N")
+		grid = grid.rotate(direction === "S" ? 2 : 1, direction === "E" ? "CCW": "CW");
+
 	for (const cell of grid) {
 		if (cell.value !== "O")
 			continue;
-		const emptyAbove = cell.findCellCluster({
-			allowDiagonal: false,
-			allowHorizontal: false,
-			allowVertical: true,
-			test: ((c) => c.value === "." && c.position[0] < cell.position[0]),
-		});
+		const north = cell.north();
+		if (north?.value !== ".")
+			continue;
+		const emptyAbove = north.findCellCluster({allowDiagonal: false, allowHorizontal: false, test: ((c) => c.value === ".")});
 		if (emptyAbove.length > 0) {
 			emptyAbove[0].setValue("O");
 			cell.setValue(".");
 		}
 	}
-	switch (direction) {
-		case "E":
-			grid = grid.rotate(1, "CW");
-			break;
-		case "W":
-			grid = grid.rotate(1, "CCW");
-			break;
-		case "S":
-			grid = grid.rotate(2, "CW");
-			break;
-	}
-	return grid;
-}
-
-function getGridRow(grid: Grid, row: number) {
-	const cells = new Array<Cell>();
-	for (let col=0; col < grid.colCount; col++) {
-		const cell = grid.getCell([row, col]);
-		if (cell?.value === "O")
-			cells.push(cell);
-	}
-	return cells;
-}
-
-function getGridColumn(grid: Grid, col: number) {
-	const cells = new Array<Cell>();
-	for (let row=0; row < grid.rowCount; row++) {
-		const cell = grid.getCell([row, col]);
-		if (cell?.value === "O")
-			cells.push(cell);
-	}
-	return cells;
-}
-
-function tiltGrid2(grid: Grid, direction: Direction) {
-	const movementDirFromDirection = () => {
-		switch (direction) {
-			case "N": return [Dir.N];
-			case "E": return [Dir.E];
-			case "W": return [Dir.W];
-			case "S": return [Dir.S];
-		}
-	};
-	const processCell = (cell: Cell) => {
-		const firstNeighbor = cell.repeatMovements(movementDirFromDirection());
-		if (!firstNeighbor || firstNeighbor.value !== ".") return;
-		const cells = firstNeighbor.findCellCluster({
-			allowDiagonal: false,
-			allowHorizontal: direction === "E" || direction === "W",
-			allowVertical: direction === "N" || direction === "S",
-			test: ((c) => c.value === "."),
-		});
-		if (cells.length) {
-			const idx = direction === "N" || direction === "W" ? 0 : cells.length - 1;
-			cells[idx].setValue("O");
-			cell.setValue(".");
-		}
-	};
-
-	if (direction == "N") {
-		for (let row = 1; row < grid.rowCount; row++) {
-			getGridRow(grid, row).forEach(processCell);
-		}
-	} else if (direction === "S") {
-		for (let row = grid.rowCount - 2; row >= 0; row--) {
-			getGridRow(grid, row).forEach(processCell);
-		}
-	} else if (direction === "E") {
-		for (let col = grid.colCount - 2; col >= 0; col--) {
-			getGridColumn(grid, col).forEach(processCell);
-		}
-	} else if (direction === "W") {
-		for (let col = 1; col < grid.colCount; col++) {
-			getGridColumn(grid, col).forEach(processCell);
-		}
-	}
+	if (direction !== "N")
+		grid = grid.rotate(direction === "S" ? 2 : 1, direction === "W" ? "CCW": "CW");
 	return grid;
 }
 
@@ -132,7 +49,7 @@ function computeScore(grid: Grid) {
 }
 
 async function p2023day14_part1(input: string, ...params: any[]) {
-	const grid = tiltGrid2(new Grid({serialized: input}), "N");
+	const grid = tiltGrid(new Grid({serialized: input}), "N");
 	const sum = computeScore(grid);
 	return sum.toString();
 }
@@ -161,7 +78,7 @@ async function p2023day14_part2(input: string, ...params: any[]) {
 		}
 
 		for (const direction of new Array<Direction>("N", "W", "S", "E")) {
-			grid = tiltGrid2(grid, direction);
+			grid = tiltGrid(grid, direction);
 		}
 		process.stdout.write(".");
 	}
