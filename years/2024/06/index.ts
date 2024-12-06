6ca77d21750acd1f1909222ae20993cdac59835d
Todd Southen <tsouthen@gmail.com>
2024-12-06 00:02:58 -0800

    2024 day 6 p1

--- years/2024/06/index.ts
+++ years/2024/06/index.ts
@@ -5,6 +5,7 @@ import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
 import { normalizeTestCases } from "../../../util/test";
+import { Cell, Dir, Grid, GridPos } from "../../../util/grid";
 
 const YEAR = 2024;
 const DAY = 6;
@@ -14,7 +15,39 @@ const DAY = 6;
 // problem url  : https://adventofcode.com/2024/day/6
 
 async function p2024day6_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const grid = new Grid({ serialized: input });
+	let currCell: Cell | undefined = grid.getCells("^")[0];
+	currCell.setValue("X");
+	let direction = Dir.N;
+	while (currCell) {
+    let nextPos: GridPos | undefined = undefined;
+		const result = currCell.repeatMovements([direction], {
+			count: (candidate) => {
+				if (!candidate) {
+					// we're done, break out of the loop
+					return false;
+				}
+				if (candidate.value === "#") {
+					return false;
+				}
+				candidate.setValue("X");
+        nextPos = [...candidate.position];
+				return true;
+			},
+		});
+    // result will be undefined when we go off the grid
+    if (result && nextPos){
+      currCell = grid.getCell(nextPos);
+    } else {
+      break;
+    }
+    if (direction === Dir.N) direction = Dir.E;
+    else if (direction === Dir.E) direction = Dir.S;
+    else if (direction === Dir.S) direction = Dir.W;
+    else if (direction === Dir.W) direction = Dir.N;
+	}
+	const positions = grid.getCells("X");
+	return positions.length;
 }
 
 async function p2024day6_part2(input: string, ...params: any[]) {
@@ -22,7 +55,21 @@ async function p2024day6_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`,
+			expected: "41",
+		},
+	];
 	const part2tests: TestCase[] = [];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
@@ -48,7 +95,7 @@ async function run() {
 	const part1Solution = String(await p2024day6_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now()
+	const part2Before = performance.now();
 	const part2Solution = String(await p2024day6_part2(input));
 	const part2After = performance.now();
 
