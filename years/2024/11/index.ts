b97f6e534b0e91cab32db49fc964f4ffaaa14f90
Todd.Southen <toddsouthenbentley@users.noreply.github.com>
2024-12-11 19:22:14 -0800

    2024 Day 11 part 1

--- years/2024/11/index.ts
+++ years/2024/11/index.ts
@@ -13,16 +13,97 @@ const DAY = 11;
 // data path    : /Users/todd/projects/advent-of-code/years/2024/11/data.txt
 // problem url  : https://adventofcode.com/2024/day/11
 
+function numDigits(n: number) {
+	return 1 + Math.floor(Math.log10(Math.abs(n)));
+}
+
+function evenNumDigits(n: number) {
+	return numDigits(n) % 2 === 0;
+}
+
+function splitStones(stones: number[]) {
+	const originalLength = stones.length;
+	let nSplits = 0;
+	let nMults = 0;
+	stones.toReversed().forEach((stone, rIndex) => {
+		const lIndex = originalLength - rIndex - 1;
+		if (stone === 0) {
+			stones[lIndex] = 1;
+		} else if (evenNumDigits(stone)) {
+			const strVal = stone.toString();
+			const half = strVal.length / 2;
+			const left = Number(strVal.substring(0, half));
+			const right = Number(strVal.substring(half));
+			stones[lIndex] = left;
+			stones.push(right);
+			nSplits++;
+		} else {
+			stones[lIndex] = stone * 2024;
+			nMults++;
+		}
+	});
+	return { nSplits, nMults };
+}
+
+function splitStones2(stones: string[]) {
+	const originalLength = stones.length;
+	let nSplits = 0;
+	let nMults = 0;
+	stones.toReversed().forEach((stone, rIndex) => {
+		const lIndex = originalLength - rIndex - 1;
+		if (stone === "0") {
+			stones[lIndex] = "1";
+		} else if (stone.length % 2 === 0) {
+			const strVal = stone.toString();
+			const half = strVal.length / 2;
+			const left = strVal.substring(0, half);
+			// let right = strVal.substring(half);
+			// while (right.startsWith("0")) right = right.substring(1);
+			const right = Number(strVal.substring(half)).toString();
+			stones[lIndex] = left;
+			stones.push(right);
+			nSplits++;
+		} else {
+			stones[lIndex] = (Number(stone) * 2024).toString();
+			nMults++;
+		}
+	});
+	return { nSplits, nMults };
+}
+
 async function p2024day11_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const count = params[0] as number ?? 25;
+	const stones = input.split(" ").map(Number);
+	for (let i = 0; i < count; i++) {
+		splitStones(stones);
+	}
+	return stones.length.toString();
 }
 
 async function p2024day11_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	// return "Not implemented";
+	const count = params[0] as number ?? 75;
+	const stones = input.split(" ").map(Number);
+	stones.length = 1;
+	for (let i = 0; i < count; i++) {
+		const { nSplits, nMults } = splitStones(stones);
+		console.log(i, stones.length, nSplits, nMults);
+	}
+	return stones.length.toString();
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{
+			input: `125 17`,
+			extraArgs: [6],
+			expected: "22"
+		},
+		{
+			input: `125 17`,
+			expected: "55312"
+		}
+	];
 	const part2tests: TestCase[] = [];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
